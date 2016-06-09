65774b3e2e5e6fcb49f9474e71daaf1021426d7f
Ben Parrish <bparrish99@gmail.com>
2016-06-08 19:33:35 -0500

    colors

--- ice/const.js
+++ ice/const.js
@@ -14,8 +14,8 @@ const TERRAIN_FLOOR = new Terrain("floor", " ", "#666", "#222", "#222", "#112", 
 const TERRAIN_WALL = new Terrain("wall", "^", "#fff", "#334", "#339", "#112", true, true, "3");
 const TERRAIN_DOOR = new Terrain("door", "+", "#c93", "#333", "#000", "#000", true, false, "4");
 const TERRAIN_OUTSIDE = new Terrain("outside", " ", "", "#333", "#00f", "#000", true, true, "7");
-const TERRAIN_DOWNSTAIRS = new Terrain("downstairs", ">", "#6f6", "#333", "#222", "#000", false, false, "8");
-const TERRAIN_UPSTAIRS = new Terrain("upstairs", "<", "#66f", "#333", "#000", "#000", false, false, "9");
+const TERRAIN_DOWNSTAIRS = new Terrain("downstairs", ">", "#6f6", "#242", "#222", "#112", false, false, "8");
+const TERRAIN_UPSTAIRS = new Terrain("upstairs", "<", "#66f", "#224", "#222", "#112", false, false, "9");
 
 const ITEM_PRIZE = new Item("prize", "%", "2", "#0f0", "#060");
 const NUM_PRIZES = 20;
